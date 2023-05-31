4ea9c1ceefb2caa425e2ce2ce92e09fd2715823f
UberChili <nunezlopez.ja@gmail.com>
2023-05-31 16:48:23 -0600

    Updated readme and starting Day9

---
+++ Day09/day09.cpp
@@ -0,0 +1,36 @@
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using std::istringstream;
+
+
+int main(int argc, char *argv[]){
+    if (argc != 2) {
+        std::cout << "Usage: ./day09 [input]\n";
+        return 1;
+    }
+
+    std::ifstream input;
+    input.open(argv[1]);
+
+    if (input.is_open()) {
+        std::string line;
+
+        while (std::getline(input, line)) {
+
+            std::cout << line << std::endl;
+        }
+
+        input.close();
+    }
+    else {
+        std::cout << "Failed to open file " << argv[1] << std::endl;
+    }
+
+    return 0;
+}
