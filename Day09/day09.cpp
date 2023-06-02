7883aba1de7ea841e140215b5cc16f14e6c21ba4
UberChili <nunezlopez.ja@gmail.com>
2023-06-01 18:41:12 -0600

    More baby steps. Some idea is being formed

--- Day09/day09.cpp
+++ Day09/day09.cpp
@@ -4,6 +4,8 @@
 #include <regex>
 #include <string>
 #include <sstream>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
 using std::istringstream;
@@ -11,9 +13,36 @@ using std::istringstream;
 
 // Data structure to store locations and their neighbors
 struct node {
-    std::vector<node> neighbors;
+    std::pair<std::string, std::string> neighbors_pair;
     int distance;
 };
+
+
+// splits a string by whitespaces and returns a vector of strings
+std::vector<std::string> split(std::string line) {
+    std::vector<std::string> tokens;
+    istringstream iss(line);
+    std::string token;
+
+
+    while (std::getline(iss, token, ' ')) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+
+// Builds a node with the neighbors data so it is easier to work with
+node make_node(std::vector<std::string> line) {
+    // neighbors= {{line.front(), line.at(2)}, 2};
+    return {{line.front(), line.at(2)}, 2};
+}
+
+
+void build_map(std::unordered_map<std::string, std::vector<std::string>>& map, std::vector<std::string> line) {
+    node neighbors = make_node(line);
+
+}
 
 
 int main(int argc, char *argv[]){
@@ -28,9 +57,12 @@ int main(int argc, char *argv[]){
     if (input.is_open()) {
         std::string line;
 
-        while (std::getline(input, line)) {
+        std::vector<std::string> splitted_line;
+        std::unordered_map<std::string, std::vector<std::string>> locations;
 
-            std::cout << line << std::endl;
+        while (std::getline(input, line)) {
+            // std::cout << line << std::endl;
+            splitted_line = split(line);
         }
 
         input.close();
