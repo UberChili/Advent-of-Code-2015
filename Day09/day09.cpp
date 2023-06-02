e19db7aba73fe127c14fa2f5a57e6e7317df2bab
UberChili <nunezlopez.ja@gmail.com>
2023-06-02 14:33:11 -0600

    Managed to do some actual work. Learned how to properly build the graph. I think this probably won't be that hard. I should just rest for now

--- Day09/day09.cpp
+++ Day09/day09.cpp
@@ -1,5 +1,6 @@
 #include <cstring>
 #include <deque>
+#include <queue>
 #include <iostream>
 #include <fstream>
 #include <regex>
@@ -14,9 +15,8 @@ using std::istringstream;
 
 // Data structure to store locations and their neighbors
 // Probably won't even be needing this, keeping it, for now
-struct node {
-    std::pair<std::string, std::string> neighbors_pair;
-    int distance;
+struct location {
+    std::vector<std::pair<std::string, int>> neighbors;
 };
 
 
@@ -34,13 +34,6 @@ std::vector<std::string> split(std::string line) {
 }
 
 
-// Builds a node with the neighbors data so it is easier to work with
-node make_node(std::vector<std::string> line) {
-    // neighbors= {{line.front(), line.at(2)}, 2};
-    return {{line.front(), line.at(2)}, 2};
-}
-
-
 // Checks vector of strings for a 'search'. Returns true if search exists in the vector
 bool contains_element(std::vector<std::string> str_vec, std::string search) {
     if (std::find(str_vec.begin(), str_vec.end(), search) != str_vec.end())
@@ -49,28 +42,17 @@ bool contains_element(std::vector<std::string> str_vec, std::string search) {
 }
 
 
-void build_map(std::unordered_map<std::string, std::string>& map, std::vector<std::string> line) {
-    if (!map.contains(line.front())) {
-        map[line.front()] = line.at(2);
-    }
-    if (!map.contains(line.at(2))) {
-        map[line.at(2)] = line.front();
-    }
+void update_map(std::unordered_map<std::string, location>& map, std::vector<std::string> line) {
+    map[line.front()].neighbors.push_back({line.at(2), static_cast<int>(std::stoul(line.back()))});
+    map[line.at(2)].neighbors.push_back({line.front(), static_cast<int>(std::stoul(line.back()))});
 }
 
-bool search(std::unordered_map<std::string, std::vector<std::string>> graph, std::string name) {
-    std::deque<std::string> search_queue;
-    std::vector<std::string> searched;
-    search_queue.insert(search_queue.end(), graph[name].begin(), graph[name].end());
 
-    while (!search_queue.empty()) {
-        std::string location = search_queue.front();
-        search_queue.pop_front();
-        if (!contains_element(searched, location)) {
-        }
+void dijkstra(std::unordered_map<std::string, location> map, std::string start) {
+
+    for (const auto& [key, value] : map) {
     }
 
-    return false;
 }
 
 
@@ -87,11 +69,21 @@ int main(int argc, char *argv[]){
         std::string line;
 
         std::vector<std::string> splitted_line;
-        std::unordered_map<std::string, std::string> locations;
+        std::unordered_map<std::string, location> locations;
 
         while (std::getline(input, line)) {
             // std::cout << line << std::endl;
             splitted_line = split(line);
+            update_map(locations, splitted_line);
+        }
+
+
+        for (const auto& [key, values] : locations) {
+            std:: cout << key << ": ";
+            for (const auto& val : values.neighbors) {
+                std::cout << val.first << " ";
+            }
+            std::cout << std::endl;
         }
 
         input.close();
