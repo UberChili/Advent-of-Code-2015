1740d94d06d81f3914e31e58dd3dfcb77971606c
UberChili <nunezlopez.ja@gmail.com>
2023-06-05 16:39:29 -0600

    Did something

--- Day09/day09.cpp
+++ Day09/day09.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstring>
 #include <deque>
 #include <queue>
@@ -13,13 +14,6 @@
 using std::istringstream;
 
 
-// Data structure to store locations and their neighbors
-// Probably won't even be needing this, keeping it, for now
-struct location {
-    std::vector<std::pair<std::string, int>> neighbors;
-};
-
-
 // splits a string by whitespaces and returns a vector of strings
 std::vector<std::string> split(std::string line) {
     std::vector<std::string> tokens;
@@ -33,7 +27,6 @@ std::vector<std::string> split(std::string line) {
     return tokens;
 }
 
-
 // Checks vector of strings for a 'search'. Returns true if search exists in the vector
 bool contains_element(std::vector<std::string> str_vec, std::string search) {
     if (std::find(str_vec.begin(), str_vec.end(), search) != str_vec.end())
@@ -41,18 +34,55 @@ bool contains_element(std::vector<std::string> str_vec, std::string search) {
     return false;
 }
 
+void update_map(std::unordered_map<std::string, std::unordered_map<std::string, int>>& graph, std::vector<std::string> line) {
+    graph[line.front()][line.at(2)] = static_cast<int>(std::stoul(line.back()));
+    graph[line.at(2)][line.front()] = static_cast<int>(std::stoul(line.back()));
+}
 
-void update_map(std::unordered_map<std::string, location>& map, std::vector<std::string> line) {
-    map[line.front()].neighbors.push_back({line.at(2), static_cast<int>(std::stoul(line.back()))});
-    map[line.at(2)].neighbors.push_back({line.front(), static_cast<int>(std::stoul(line.back()))});
+
+bool pair_compare(std::pair<std::string, int> a, std::pair<std::string, int> b) {
+    return a.second < b.second;
+}
+
+
+std::string get_cheapest(std::unordered_map<std::string, int> map) {
+    const auto result = std::min_element(map.begin(), map.end(), pair_compare);
+    std::cout << "Min element is: " << result->first << " with val: " << result->second << std::endl;
+
+    return result->first;
+}
+
+
+void printpath(std::unordered_map<std::string, std::unordered_map<std::string, int>> graph, std::string start) {
+    // std::deque<std::string> search_queue;
+    int total = 0;
+    std::vector<std::string> searched;
+    searched.push_back(start);
+
+    for (const auto& [key, val] : graph) {
+        std::cout << key << ": \n";
+        std::string current = get_cheapest(val);
+        if (!contains_element(searched, current)) {
+            total += graph[key][current];
+            searched.push_back(current);
+        }
+    }
+
+    std::cout << "\nRoute: \n";
+    for (const auto& i : searched) {
+        std::cout << i << " ";
+    }
+    std::cout << "\nDistance: " << total << std::endl;
+
 }
 
 
 int main(int argc, char *argv[]){
-    if (argc != 2) {
-        std::cout << "Usage: ./day09 [input]\n";
+    if (argc != 3) {
+        std::cout << "Usage: ./day09 [input] [start]\n";
         return 1;
     }
+    std::string start = argv[2];
 
     std::ifstream input;
     input.open(argv[1]);
@@ -61,7 +91,8 @@ int main(int argc, char *argv[]){
         std::string line;
 
         std::vector<std::string> splitted_line;
-        std::unordered_map<std::string, location> locations;
+        // std::unordered_map<std::string, location> locations;
+        std::unordered_map<std::string, std::unordered_map<std::string, int>> locations;
 
         while (std::getline(input, line)) {
             // std::cout << line << std::endl;
@@ -69,14 +100,22 @@ int main(int argc, char *argv[]){
             update_map(locations, splitted_line);
         }
 
+        // for (const auto& [key, values] : locations) {
+        //     std:: cout << key << ": ";
+        //     for (const auto& val : values) {
+        //         std::cout << val.second << " ";
+        //     }
+        //     std::cout << std::endl;
+        // }
+        printpath(locations, start);
 
-        for (const auto& [key, values] : locations) {
-            std:: cout << key << ": ";
-            for (const auto& val : values.neighbors) {
-                std::cout << val.first << " ";
-            }
-            std::cout << std::endl;
-        }
+        // for (const auto& [key, values] : locations) {
+        //     std:: cout << key << ": ";
+        //     for (const auto& val : values.neighbors) {
+        //         std::cout << val.first << " ";
+        //     }
+        //     std::cout << std::endl;
+        // }
 
         input.close();
     }
